9765a885c4b9c8637ed0e97debbf85cffef50c81
Sumi Seo <sumseo@paul-f4Br6s6.clusters.42paris.fr>
2025-03-19 16:57:49 +0100

    feat/mariadb

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "CREATE DATABASE IF NOT EXISTS"
+set -e  # Exit immediately if a command exits with a non-zero status
+
+echo "Starting MariaDB..."
+# Add any commands you want to run, for example:
+# /etc/init.d/mysql start  (or whichever method you use to start MariaDB)
+# Check if MariaDB starts successfully
+
+# Here is an example of a command to start MariaDB:
+exec mysqld --user=mysql
